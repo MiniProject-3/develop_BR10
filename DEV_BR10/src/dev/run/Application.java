f5611b4063637d84ed31ee1d5916876125301b5b
Hansoo <orca2inus@gmail.com>
2021-08-20 23:50:00 +0900

    21.08.20-HS-OD-0.2 사이즈 카테고리 GUI 틀 완성

--- DEV_BR10/src/dev/run/Application.java
+++ DEV_BR10/src/dev/run/Application.java
@@ -7,11 +7,11 @@ public class Application {
 
 	public static void main(String[] args) {
 
-		gui odersize = new gui();
 		
-		odersize.OrderSize();
+		gui ordersize = new gui();
 		
 		
+		ordersize.OrderSize();
 		
 		
 		
