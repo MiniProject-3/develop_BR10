e043bc9bf05c417ffb72ce4e51c8c91f4fd2c707
김주호 <@DESKTOP-6I4CEJ9>
2021-08-20 23:15:13 +0900

    21082023_gui 음료 추가

--- DEV_BR10/src/dev/customer/gui/Application.java
+++ DEV_BR10/src/dev/customer/gui/Application.java
@@ -7,12 +7,14 @@ public class Application {
 //		gui ordersize = new gui();
 //		gui_icecream icecream = new gui_icecream();
 //		gui_cake cake = new gui_cake();
-		gui_desert desert = new gui_desert();
+//		gui_desert desert = new gui_desert();
+		gui_drink drink = new gui_drink();
 		
 //		ordersize.OrderSize();
 //		icecream.Icecream();
 //		cake.cake();
-		desert.desert();
+//		desert.desert();
+		drink.drink();
 		
 		
 	}
---
+++ DEV_BR10/src/dev/customer/gui/gui_drink.java
@@ -0,0 +1,52 @@
+package dev.customer.gui;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class gui_drink {
+	private JFrame mf = new JFrame("BR_10");
+	   public void drink() {
+		  mf.setTitle("음료");
+	      mf.setBounds(600, 10, 600, 1000);
+	      mf.setLayout(null);
+	      
+	      JPanel panel = new JPanel();
+	      panel.setLocation(0, 100);
+	      panel.setSize(700,750);
+	      panel.setBackground(Color.WHITE);
+	      
+	      JPanel drink = new JPanel();
+	      drink.setLocation(100, 200);
+	      drink.setSize(400, 500);
+	      drink.setLayout(new GridLayout(2,2));
+	      
+	      
+	      String[] size = {"바닐라 라떼", "아메리카노", "아이스 바닐라 라떼", "아이스 아메리카노"};
+	      
+	      JButton[] butt = new JButton[size.length];
+	      
+	      
+	      for (int i = 0; i < butt.length; i++) {
+	    	  butt[i] = new JButton(size[i]);
+			}
+	      
+	      for (int i = 0; i < butt.length; i++) {
+
+	    	  drink.add(butt[i]);
+
+			}
+	      
+	      
+	      mf.add(drink);
+	      
+	      
+	      mf.add(panel);
+	      
+	      mf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	      mf.setVisible(true);
+	   }
+}
