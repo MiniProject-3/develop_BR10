4e1cc28592dcc8c65349af818d7e0b6f5c448253
김주호 <@DESKTOP-6I4CEJ9>
2021-08-20 23:03:53 +0900

    21082023_gui 케이크 추가

--- DEV_BR10/src/dev/customer/gui/Application.java
+++ DEV_BR10/src/dev/customer/gui/Application.java
@@ -5,10 +5,12 @@ public class Application {
 	public static void main(String[] args) {
 
 //		gui ordersize = new gui();
-		gui_icecream icecream = new gui_icecream();
+//		gui_icecream icecream = new gui_icecream();
+		gui_cake cake = new gui_cake();
 		
 //		ordersize.OrderSize();
-		icecream.Icecream();
+//		icecream.Icecream();
+		cake.cake();
 		
 	}
 
