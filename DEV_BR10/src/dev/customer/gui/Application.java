db065d5eba798f9bf210e2224b71b4e0efc369cc
김주호 <@DESKTOP-6I4CEJ9>
2021-08-20 22:48:01 +0900

    21082022_아이스크림 gui추가

---
+++ DEV_BR10/src/dev/customer/gui/Application.java
@@ -0,0 +1,15 @@
+package dev.customer.gui;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+		gui ordersize = new gui();
+		gui_icecream icecream = new gui_icecream();
+		
+//		ordersize.OrderSize();
+		icecream.Icecream();
+		
+	}
+
+}
