7185d089414945cfcf3fcd971778c91797f07682
Hansoo <orca2inus@gmail.com>
2021-08-21 15:00:15 +0900

    210821_15 매장포장 일부완성

--- DEV_BR10/src/dev/customer/gui/Application.java
+++ DEV_BR10/src/dev/customer/gui/Application.java
@@ -5,14 +5,20 @@ public class Application {
 	public static void main(String[] args) {
 
 		gui ordersize = new gui();
-		gui_md md = new gui_md();
+		
+		
+		
+//		gui_md md = new gui_md();
+		gui_hereOrToGo hto = new gui_hereOrToGo();
 //		gui_icecream icecream = new gui_icecream();
 //		gui_cake cake = new gui_cake();
 //		gui_desert desert = new gui_desert();
 //		gui_drink drink = new gui_drink();
 		
 //		ordersize.OrderSize();
-		md.md();
+//		md.md();
+		hto.hotg();
+		
 //		icecream.Icecream();
 //		cake.cake();
 //		desert.desert();
--- DEV_BR10/src/dev/customer/gui/gui_hereOrToGo.java
+++ DEV_BR10/src/dev/customer/gui/gui_hereOrToGo.java
@@ -8,10 +8,10 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class gui_md {
+public class gui_hereOrToGo {
 	private JFrame mf = new JFrame("BR_10");
 
-	public void md() {
+	public void hotg() {
 		mf.setTitle("mini_BR10");
 		mf.setBounds(600, 10, 600, 1000);
 		mf.setLayout(null);
@@ -37,8 +37,14 @@ public class gui_md {
 		controllPanel.setSize(600, 50);
 		controllPanel.setBackground(Color.WHITE);
 		
+		JPanel controllPanel2 = new JPanel();
+		controllPanel2.setLayout(null);
+		controllPanel2.setLocation(0, 750);
+		controllPanel2.setSize(600, 50);
+		controllPanel2.setBackground(Color.WHITE);
+
 		
-		String[] controll = { "이전", "장바구니", "결제" };
+		String[] controll = { "이전", "장바구니", "결제", "이전" };
 
 		JButton[] butt4 = new JButton[controll.length];
 
@@ -57,37 +63,37 @@ public class gui_md {
 		butt4[1].setSize(50, 50);
 		butt4[2].setLocation(450, 0);
 		butt4[2].setSize(100, 50);
+		butt4[3].setLocation(50, 0);
+		butt4[3].setSize(100, 50);
 		
-		JPanel mdGoods = new JPanel();
-		mdGoods.setLocation(125, 250);
-		mdGoods.setSize(350, 450);
-		mdGoods.setLayout(null);
-		mdGoods.setBackground(Color.WHITE);
+		controllPanel2.add(butt4[3]);
+		
+		JPanel hgPanel = new JPanel();
+		hgPanel.setLocation(125, 250);
+		hgPanel.setSize(350, 450);
+		hgPanel.setLayout(null);
+//		hgPanel.setBackground(Color.WHITE);
 
-		String[] md_butt = { "핑크", "블루", "건너뛰기", "추가하기","md상품 취소" };
-		JButton[] butt = new JButton[md_butt.length];
+		String[] hg = { "매장", "포장"};
+		JButton[] butt = new JButton[hg.length];
 
 		for (int i = 0; i < butt.length; i++) {
-			butt[i] = new JButton(md_butt[i]);
+			butt[i] = new JButton(hg[i]);
 		}
 
 		for (int i = 0; i < butt.length; i++) {
-			mdGoods.add(butt[i]);
+			hgPanel.add(butt[i]);
 		}
-		butt[0].setLocation(50, 200);
+		butt[0].setLocation(60, 150);
 		butt[0].setSize(100, 100);
-		butt[1].setLocation(200, 200);
+		butt[1].setLocation(190, 150);
 		butt[1].setSize(100, 100);
-		butt[2].setLocation(25, 350);
-		butt[2].setSize(125, 30);
-		butt[3].setLocation(200, 350);
-		butt[3].setSize(125, 30);
-		butt[4].setLocation(310, 15);
-		butt[4].setSize(25, 25);
+
 		
 		mf.add(homePanel);
-		mf.add(mdGoods);
-		mf.add(controllPanel);
+		mf.add(hgPanel);
+//		mf.add(controllPanel);
+		mf.add(controllPanel2);
 		mf.add(panel);
 
 		mf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
