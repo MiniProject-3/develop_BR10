fad3ce9d73325be5fe57039de4637311ef636d8c
JH_OD_2.0 <kjh00345@gmail.com>
2021-08-26 19:02:17 +0900

    21082619_매장,포장 버튼 연결

--- DEV_BR10/src/dev/customer/gui/Gui_payment.java
+++ DEV_BR10/src/dev/customer/gui/Gui_payment.java
@@ -9,56 +9,57 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Gui_payment {
-	private JFrame mf = new JFrame("BR_10");
-	private ImageKick kb = new ImageKick();
+public class Gui_payment extends JPanel{
+   private JFrame mf = new JFrame("BR_10");
+   private ImageKick kb = new ImageKick();
+   
 
-	public void payment() {
-		mf.setTitle("mini_BR10");
-		mf.setBounds(600, 10, 600, 1000);
-		mf.setLayout(null);
+   public Gui_payment() {
+      mf.setTitle("mini_BR10");
+      mf.setBounds(600, 10, 600, 1000);
+      mf.setLayout(null);
 
-		ImageKickPanel panel = new ImageKickPanel(
-				new ImageIcon("BR10_images/gui_order/bg_06.jpg").getImage().getScaledInstance(600, 1000, 3));
-		panel.setLocation(0, 0);
-		panel.setSize(600, 1000);
-		panel.setBackground(Color.WHITE);
+      ImageKickPanel panel = new ImageKickPanel(
+            new ImageIcon("BR10_images/gui_order/bg_06.jpg").getImage().getScaledInstance(600, 1000, 3));
+      panel.setLocation(0, 0);
+      panel.setSize(600, 1000);
+      panel.setBackground(Color.WHITE);
 
-		JPanel homePanel = new JPanel();
-		homePanel.setLocation(50, 130);
-		homePanel.setSize(50, 50);
-		homePanel.setBackground(Color.WHITE);
+      JPanel homePanel = new JPanel();
+      homePanel.setLocation(50, 130);
+      homePanel.setSize(50, 50);
+      homePanel.setBackground(Color.WHITE);
 
-		JButton butt2 = new JButton("ÌôàÎ≤ÑÌäº");
-		butt2.setPreferredSize(new Dimension(50, 50));
-		homePanel.add(butt2);
+      JButton butt2 = new JButton("»®πˆ∆∞");
+      butt2.setPreferredSize(new Dimension(50, 50));
+      homePanel.add(butt2);
 
-		JPanel PaymentPanel = new JPanel();
-		PaymentPanel.setLocation(125, 250);
-		PaymentPanel.setSize(350, 450);
-		PaymentPanel.setLayout(null);
-		PaymentPanel.setBackground(Color.WHITE);
+      JPanel PaymentPanel = new JPanel();
+      PaymentPanel.setLocation(125, 250);
+      PaymentPanel.setSize(350, 450);
+      PaymentPanel.setLayout(null);
+      PaymentPanel.setBackground(Color.WHITE);
 
-		String[] md_butt = { "Í∏∞ÌîÑÌã∞ÏΩò", "Ìú¥ÎåÄÌè∞ Í≤∞Ï†ú" };
-		JButton[] butt = new JButton[md_butt.length];
+      String[] md_butt = { "±‚«¡∆ºƒ‹", "»ﬁ¥Î∆˘ ∞·¡¶" };
+      JButton[] butt = new JButton[md_butt.length];
 
-		for (int i = 0; i < butt.length; i++) {
-			butt[i] = new JButton(md_butt[i]);
-		}
+      for (int i = 0; i < butt.length; i++) {
+         butt[i] = new JButton(md_butt[i]);
+      }
 
-		for (int i = 0; i < butt.length; i++) {
-			PaymentPanel.add(butt[i]);
-		}
-		butt[0].setLocation(50, 150);
-		butt[0].setSize(250, 50);
-		butt[1].setLocation(50, 250);
-		butt[1].setSize(250, 50);
+      for (int i = 0; i < butt.length; i++) {
+         PaymentPanel.add(butt[i]);
+      }
+      butt[0].setLocation(50, 150);
+      butt[0].setSize(250, 50);
+      butt[1].setLocation(50, 250);
+      butt[1].setSize(250, 50);
 
-		mf.add(homePanel);
-		mf.add(PaymentPanel);
-		mf.add(panel);
+      mf.add(homePanel);
+      mf.add(PaymentPanel);
+      mf.add(panel);
 
-		mf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		mf.setVisible(true);
-	}
+      mf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      mf.setVisible(true);
+   }
 }
