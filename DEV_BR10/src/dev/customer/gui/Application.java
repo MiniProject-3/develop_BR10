f0dda10bd62b2753eb63140db8a2689f24db6767
Hansoo <orca2inus@gmail.com>
2021-08-27 23:09:08 +0900

    210827_23 장바구니 완료

--- DEV_BR10/src/dev/customer/gui/Application.java
+++ DEV_BR10/src/dev/customer/gui/Application.java
@@ -6,10 +6,13 @@ public class Application {
 
 	public static void main(String[] args) {
 
-//  	new StartMain();
+  	new StartMain();
 //  	
-		Gui_gifticon af = new Gui_gifticon();
-				af.gift();
+//		Gui_gifticon af = new Gui_gifticon();
+//				af.gift();
+				
+//				Gui_basket as = new Gui_basket();
+//				as.Gui_basket();
 				
 //  	Gui_waitingNum as =new Gui_waitingNum();
 //  				as.waitingNum();
