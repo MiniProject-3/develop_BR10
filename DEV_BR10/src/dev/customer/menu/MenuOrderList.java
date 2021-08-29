f39da883072108c5f2cbb28aa70886388188ff83
Hansoo <orca2inus@gmail.com>
2021-08-29 20:55:27 +0900

    210829_21 케이크 DAO

---
+++ DEV_BR10/src/dev/customer/menu/MenuOrderList.java
@@ -0,0 +1,14 @@
+package dev.customer.menu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import dev.dto.OrderDTO;
+
+public class MenuOrderList {
+	
+	
+	public static List<OrderDTO> orderList = new ArrayList<>();
+	
+	
+}
